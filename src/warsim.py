bd9000872adf30975f7a6e456c72ff8f5e35532d
Teresa [Redacted] <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-02-09 13:05:34 -0500

    Update warsim.py

--- src/warsim.py
+++ src/warsim.py
@@ -1,4 +1,12 @@
 import io
 import math
 
-print('Welcome to Warsim')
+print('Welcome to Warsim\n\n\n')
+### TODO: Add saving and loading
+print('1) New Game\n')
+print('2) Load game\n')
+print('3) Quit to desktop\n')
+if input() == '3':
+    quit()
+elif input() == '2' or input() == '1':
+    print('To be added')
